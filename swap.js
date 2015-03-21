f6cd71fb72cbf1b63fc3271d8aab552a9dfd0e64
unknown <jo.jcat@gmail.com>
2015-03-20 19:18:49 -0700

    Double size of arrows for votes.

---
+++ swap.js
@@ -0,0 +1,10 @@
+
+//var upArrow = document.getElementById("mainUpArrow");
+//var downArrow = document.getElementById("mainDownArrow");
+
+// upvote-image btn btn-grey left title arrow up pushed
+// pushed is removed when not selected
+
+
+
+
